eedc9b45c0cc80609aabe581dd8bc3bf7723b7ac
Shota Miyazaki <jmsf0203@gmail.com>
2014-06-21 22:56:21 +0900

    added comment

--- ren3.rb
+++ ren3.rb
@@ -1,6 +1,6 @@
 load("parses2.rb")
 
-def makeShikigi(ar)
+def makeShikigi(ar)#与えられた数字の配列に対して、それを葉とする全ての式木を作る
   shikis = ["+","-","*","/"]
   n = ar.length
   a = []
@@ -9,8 +9,11 @@ def makeShikigi(ar)
   else
     for i in 1..n-1
       makeShikigi(ar[0...i]).each do |lnodes|
-        makeShikigi(ar[i..-1]).each do |rnodes|
+      #左側の個数iに対して可能な式木と
+        makeShikigi(ar[i...n]).each do |rnodes|
+        #右側の個数n-iに対して可能な式木を組み合わせる。
           shikis.each do |shiki|
+            #演算子それぞれを用いる。
             a << ParseNodes.new(shiki,lnodes,rnodes)
           end
         end
@@ -24,15 +27,17 @@ def find10(n)
   numbers = (0..9).to_a
   a = []
   numbers.repeated_combination(n) do |combi|
+    #combiは0から9までの重複ありのnこの配列
     catch(:ok){
-      combi.permutation do |permu|
+      combi.permutation do |permu| #combiを並べ替えた全て
         makeShikigi(permu).each do |shikigi|
-          if shikigi.value == 10
+          if shikigi.value == 10 #値が10になるものがあればok
             p shikigi.inorder
             throw(:ok)
           end
         end
       end
+      #なかったらaに加える。
       puts combi.to_s + "can't be 10."
       a << combi
     }
